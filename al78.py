de86ea2418f7c21983b614a0eb75c7d947a60c66
OnurynFFc <149428021+OnurynFFc@users.noreply.github.com>
2026-01-30 22:46:22 -0300

    Metodos_dicionarios

--- al78.py
+++ al78.py
@@ -4,7 +4,7 @@ Docstring for al78
 Métodos de dicionários em Python
 len() - Retorna o número de itens no dicionário
 clear() - Remove todos os itens do dicionário
-copy() - Retorna uma cópia rasa do dicionário
+copy() - Retorna uma cópia rasa do dicionário (shallow copy) 
 values() - Retorna uma visão dos valores no dicionário
 items() - Retorna uma visão dos pares chave-valor no dicionário
 keys() - Retorna uma visão das chaves no dicionário
@@ -14,14 +14,51 @@ popitem() - Remove e retorna um par chave-valor arbitrário do dicionário
 update() - Atualiza o dicionário com os pares chave-valor de outro dicionário ou de um iterável de pares chave-valor
 setdefault() - Retorna o valor da chave especificada. Se a chave não existir
 """
-pessoa = {
-    'nome': 'Ana',
-    'idade': 30,
-    'cidade': 'São Paulo'
+
+p1 ={
+    'nome': 'Luiz',
+    'sobrenome': 'Otávio',
 }
 
-pessoa.setdefault('sobrenome', 'souza')
-print(pessoa['sobrenome'])
+
+p1.update({'nome': 'João', 'idade': 30})  # Atualiza o dicionário com novos pares chave-valor
+print(p1)
+
+
+
+# sobrenome = p1.popitem()  # Remove e retorna um par chave-valor arbitrário
+# print(sobrenome)  # ('sobrenome', 'Otávio')
+# print(p1)
+# # nome = p1.pop('nome')  # Remove a chave 'nome' e retorna o valor associado
+# print(nome)  # Luiz
+
+# print(p1.get('nome', 'Chave não encontrada'))  # Luiz
+
+
+# import copy
+
+# d1 = {
+#     'c1':1,
+#     'c2':2,
+#     'l1':[10,20,30],
+# }
+# d2 = copy.deepcopy(d1)  # Cópia profunda
+
+# d2['c1'] = 1000
+# d2['l1'][0] = 9999
+
+
+# print(d1)
+# print(d2)
+
+# pessoa = {
+#     'nome': 'Ana',
+#     'idade': 30,
+#     'cidade': 'São Paulo'
+# }
+
+# pessoa.setdefault('sobrenome', 'souza')
+# print(pessoa['sobrenome'])
 
 
 # print(len(pessoa))  # Retorna o número de itens no dicionário
