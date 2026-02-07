d766364a183a01e3566bbd5970909d4f4a429e22
OnurynFFc <149428021+OnurynFFc@users.noreply.github.com>
2026-02-07 16:02:43 -0300

    Sets

--- al80.py
+++ al80.py
@@ -16,13 +16,27 @@ remove() - Remove um elemento do set (gera erro se o elemento não existir)
 discard() - Remove um elemento do set (não gera erro se o elemento não existir)
 clear() - Remove todos os elementos do set
 union() - Retorna a união de dois sets
-
  cria um set vazio
 # criando um set com elementos
 s2 = {1, 2, 3, 4}  # Duplicatas serão removidas
 print(s2)  # Saída: {1, 2, 3, 4}
-
 '''
-s1 = set()  # Criação de um set vazio
-s1 = {'Lipe',1,2,3}  # Criação de um set com elementos
+# l1 = [1, 2, 3, 3, 3, 3, 3, 3, 3, 1]  # Lista com elementos duplicados
+# s1 = set(l1)
+# l2 = list(s1)
+# print(l2)  # Saída: {1, 2, 3} - Duplicatas são removidas
 
+s1 ={1,2,3}
+# print(3  in s1)
+
+for numero in s1:
+    print(numero)
+
+# Métodos úteis:
+# add, update, clear, discard
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
