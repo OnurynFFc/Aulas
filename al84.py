3e29058cdfb81a9b62f8617ee294c1dc70862477
OnurynFFc <149428021+OnurynFFc@users.noreply.github.com>
2026-04-22 20:34:34 -0300

    retorno

--- al84.py
+++ al84.py
@@ -3,6 +3,12 @@
 
 # print(list(range(10)))
 
+import pprint
+def p(v):
+    pprint.pprint(novos_produtos, sort_dicts=False, width=40)
+
+    
+
 lista = []
 for numero in range(10):
     lista.append(numero)
@@ -11,3 +17,25 @@ lista = [
     numero * 2 for numero in range(10)
     ]
 print(lista)
+
+
+#Mapeamento de dados em list comprehension
+
+produtos = [
+    {'nome': 'p1', 'preco': 20},
+    {'nome': 'p2', 'preco': 10},
+    {'nome': 'p3', 'preco': 30}
+]
+
+novos_produtos =[
+    
+    {**produto, 'preco':produto['preco']*1.05}
+    if produto['preco']>20 else {**produto}
+    for produto in produtos
+    if (produto['preco'] >= 20 and produto['preco']*1.05)>10
+]
+
+# print(*novos_produtos, sep='\n')
+p(novos_produtos)
+
+#  
