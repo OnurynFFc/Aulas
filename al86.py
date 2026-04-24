8b1aa88793e6ec1c8b6e6fc076fccb117c9491a3
OnurynFFc <149428021+OnurynFFc@users.noreply.github.com>
2026-04-24 16:46:34 -0300

    sexta

---
+++ al86.py
@@ -0,0 +1,31 @@
+#Dicitionary Comprehehnsion e Set Comprehension
+produto = {
+    'nome': 'Caneta azul',
+    'preco': 2.5,
+    'categoria': 'Escritório',
+}
+
+
+dc = {
+    chave: valor.upper()
+    if isinstance(valor, str) else valor
+    for chave, valor
+    in produto.items()
+    if chave != 'categoria'
+}
+
+lista = [
+    ('a', 'valor a'),
+    ('b', 'valor b'),
+    ('c', 'valor c'),
+]
+
+# dc= {
+#     chave: valor
+#     for chave, valor in lista
+# }
+
+# print(dict(lista))
+
+s1={2 ** i for i in range(10)}
+print(s1)
